3a9b1db929027c84ade77815af2aa1d59e705a28
Cheng Vue <cvu3@ucsc.edu>
2018-11-27 17:31:14 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,22 @@
+class CashRegister
+  attr_accessor :total, :discount
 
+  def intitalize(discount = 0)
+    @total = 0
+    @discount = discount
+  end
+
+  def add_item(title, price, quantity = 0)
+    self.total += price
+    self.item << title
+  end
+
+  def apply_discount
+    discount = @discount.percent_of(@total)
+    @total -= discount
+  end
+
+  def items
+    []
+  end
+end
